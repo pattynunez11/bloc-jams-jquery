604b636b23a8ac1106d6dccb83db39c675ac0338
Patty Nunez <nunezmacias@gmail.com>
2019-07-21 21:31:07 -0700

    Add updated content to album-info.js script

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -2,4 +2,6 @@
 {
     $('#album-title').text(album.title);
     $('img#album-cover-art').attr('src', album.albumArtUrl);
+    $('.artist').text(album.artist);
+    $('#release-info').text(album.releaseInfo);
 }
