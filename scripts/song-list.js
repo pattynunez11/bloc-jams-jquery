a07142501b3295a3f84d3e39d50a7b53ae4218b3
Patty Nunez <nunezmacias@gmail.com>
2019-07-24 12:12:19 -0700

    Add acceptance criteria code for songlist| play pause

---
+++ scripts/song-list.js
@@ -0,0 +1,25 @@
+//creating(element)a row for each song's data in album 
+{album.songs.forEach ( (song, index) => {
+    song.element = $(`
+    <tr>
+       <td>
+         <button>
+          <span class="song-number">${index + 1}</span>
+          <span class="ion-play"></span>
+          <span class="ion-pause"></span>
+         </button>
+        </td>
+        <td> ${song.title} </td>
+        <td> ${song.duration} </td>
+        </td> 
+     </tr>
+    `);
+//playPause from player object
+    song.element.on('click', event => {
+        player.playPause(song);
+      });
+//add text from album, song's data, into HTML    
+    $('#song-list').append(song.element);
+});
+
+}
