643cb0f3b0008acdab2e4c7d4e90839a6a8baddc
Patty Nunez <nunezmacias@gmail.com>
2019-07-25 18:00:18 -0700

    Update removal of playerPause on Next

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,21 +1,19 @@
 {
-//pause button hidden-css, switch icon using playstate attribute
-    $('button#play-pause').on('click', function(){
+//Pause button -hidden-css, switch icon using playstate attribute
+ $('button#play-pause').on('click', function(){
      player.playPause(); 
      $(this).attr('playState', player.playState);  
-    });
-//play next song, array + 1 to make a var to hold #index to call to var to play
-$('button#next').on('click', function() {
+ });
+//Next button -create index for song currently playing create cont nextSong using current song +1 to for player to play.
+ $('button#next').on('click', function() {
     if (player.playState !== 'playing')
      { return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-     const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
+     const nextSongIndex = currentSongIndex + 1;
+     const nextSong = album.songs[nextSongIndex];   
 //fixes -no next song on the last #index
     if (nextSongIndex >= album.songs.length) 
      { return; }
     player.playPause(nextSong);
-   
-});
+ });
 }
